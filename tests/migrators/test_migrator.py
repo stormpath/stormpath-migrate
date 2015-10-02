88f520bc5287594189bb2f02c0c7144331f8668f
Randall Degges <r@rdegges.com>
2015-10-02 16:47:05 -0700

    Updating tests.

--- tests/migrators/test_migrator.py
+++ tests/migrators/test_migrator.py
@@ -1,10 +1,10 @@
-"""Tests for our Migrator class."""
+"""Tests for our TenantMigrator class."""
 
 
 from os import environ
 from unittest import TestCase
 
-from migrate.migrator import Migrator
+from migrate.migrators import TenantMigrator
 
 
 # Necessary environment variables.
@@ -14,31 +14,31 @@ DST_CLIENT_ID = environ['DST_CLIENT_ID']
 DST_CLIENT_SECRET = environ['DST_CLIENT_SECRET']
 
 
-class MigratorTest(TestCase):
+class TenantMigratorTest(TestCase):
     def setUp(self):
         self.src = '%s:%s' % (SRC_CLIENT_ID, SRC_CLIENT_SECRET)
         self.dst = '%s:%s' % (DST_CLIENT_ID, DST_CLIENT_SECRET)
 
     def test_init(self):
-        migrator = Migrator('id:secret', 'xxx:yyy')
+        migrator = TenantMigrator('id:secret', 'xxx:yyy')
         self.assertEqual(migrator.src, 'id:secret')
         self.assertEqual(migrator.dst, 'xxx:yyy')
         self.assertEqual(migrator.from_date, None)
 
     def test_repr(self):
-        migrator = Migrator('id:secret', 'xxx:yyy')
+        migrator = TenantMigrator('id:secret', 'xxx:yyy')
         self.assertTrue('id:secret' in migrator.__repr__())
         self.assertTrue('xxx:yyy' in migrator.__repr__())
 
     def test_validate_credentials(self):
         with self.assertRaises(ValueError):
-            Migrator('hi', 'there')
+            TenantMigrator('hi', 'there')
 
         with self.assertRaises(ValueError):
-            Migrator(':secret', 'xxx:yyy')
+            TenantMigrator(':secret', 'xxx:yyy')
 
     def test_create_clients(self):
-        migrator = Migrator(self.src, self.dst)
+        migrator = TenantMigrator(self.src, self.dst)
         migrator._create_clients()
         self.assertTrue(migrator.src_client)
         self.assertTrue(migrator.dst_client)
