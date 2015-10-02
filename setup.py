0da3ae8ceef29573dee03eb05094b0e7d6114dd0
Randall Degges <r@rdegges.com>
2015-10-02 13:18:49 -0700

    Adding pytest-cov.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     packages = find_packages(exclude=['tests*']),
     install_requires = ['docopt', 'stormpath'],
     extras_require = {
-        'test': ['pytest', 'python-coveralls'],
+        'test': ['pytest', 'pytest-cov', 'python-coveralls'],
     },
     entry_points = {
         'console_scripts': [
