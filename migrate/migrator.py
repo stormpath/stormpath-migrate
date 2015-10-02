adddebb10165a7198046ceebb2ff9ce16ffa91c9
Randall Degges <r@rdegges.com>
2015-10-02 16:15:50 -0700

    Creating some clients.

--- migrate/migrator.py
+++ migrate/migrator.py
@@ -13,6 +13,7 @@ class Migrator(object):
         self.dst = dst
         self.from_date = from_date
         self._validate_credentials()
+        self._create_clients()
 
     def _validate_credentials(self):
         """
@@ -29,6 +30,16 @@ class Migrator(object):
             if len(id) == 0 or len(secret) == 0:
                 raise ValueError('Invalid credentials specified. Use ' \
                         '<id:secret> format.')
+
+    def _create_clients(self):
+        """
+        Create our local Stormpath Client objects used for the migration.
+        """
+        src_id, src_secret = self.src.split(':')
+        dst_id, dst_secret = self.dst.split(':')
+
+        self.src_client = Client(id=src_id, secret=src_secret)
+        self.dst_client = Client(id=dst_id, secret=dst_secret)
 
     def __repr__(self):
         return 'Migrator(<%s> -> <%s>)' % (self.src, self.dst)
