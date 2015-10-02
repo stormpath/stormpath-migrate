becb2bbd9727ae5902887f0365c50c2cb6cd81a9
Randall Degges <r@rdegges.com>
2015-10-02 12:52:12 -0700

    Adding a CLI.

---
+++ migrate/cli.py
@@ -0,0 +1,41 @@
+"""
+stormpath-migrate
+
+Usage:
+  stormpath-migrate <src> <dst> [(-f <date> | --from <date>)]
+  stormpath-migrate -h | --help
+  stormpath-migrate --version
+
+Options:
+  -h --help                         Show this screen.
+  -v                                Show verbose output.
+  --version                         Show version.
+  -i <id> | --id <id>               Stormpath API key id.
+  -s <secret> | --secret <secret>   Stormpath API key secret.
+  -f <date> | --from <date>         Only migrate resources created >= this date.  [ex: 2010-01-03]
+
+Example:
+  stormpath-migrate <id:secret> <id:secret>     # Migrate from src tenant to dst tenant.
+  stormpath-migrate <id:secret> <id:secret> --from 2010-01-01
+                                                # Migrate only resources from src tenant to dst tenant
+                                                # created on or after 2010-01-01.
+
+Help:
+  For help using this tool, please contact Stormpath support:
+  support@stormpath.com
+"""
+
+
+from docopt import docopt
+
+from . import __version__ as VERSION
+from .migrator import Migrator
+
+
+def main():
+    args = docopt(__doc__, version=VERSION)
+    migrator = Migrator(
+        src = args['<src>'],
+        dst = args['<dst>'],
+        from_date = args['--from']
+    )
