d1f15afaaabf9979c13abcc5faa9f03930258310
Randall Degges <r@rdegges.com>
2015-10-04 18:21:21 -0700

    Updating BaseMigrator test for refactoring.

--- tests/migrators/test_base.py
+++ tests/migrators/test_base.py
@@ -16,33 +16,19 @@ DST_CLIENT_SECRET = environ['DST_CLIENT_SECRET']
 
 class BaseMigratorTest(TestCase):
     def setUp(self):
-        self.src = '%s:%s' % (SRC_CLIENT_ID, SRC_CLIENT_SECRET)
-        self.dst = '%s:%s' % (DST_CLIENT_ID, DST_CLIENT_SECRET)
+        self.src = 'blah'
+        self.dst = 'blah'
 
     def test_init(self):
-        migrator = BaseMigrator('id:secret', 'xxx:yyy')
-        self.assertEqual(migrator.src, 'id:secret')
-        self.assertEqual(migrator.dst, 'xxx:yyy')
+        migrator = BaseMigrator(self.src, self.dst)
+        self.assertEqual(migrator.src, self.src)
+        self.assertEqual(migrator.dst, self.dst)
         self.assertEqual(migrator.from_date, None)
         self.assertEqual(migrator.verbose, False)
 
-        migrator = BaseMigrator('id:secret', 'xxx:yyy', verbose=True)
+        migrator = BaseMigrator(self.src, self.dst, verbose=True)
         self.assertEqual(migrator.verbose, True)
 
     def test_repr(self):
-        migrator = BaseMigrator('id:secret', 'xxx:yyy')
-        self.assertTrue('id:secret' in migrator.__repr__())
-        self.assertTrue('xxx:yyy' in migrator.__repr__())
-
-    def test_validate_credentials(self):
-        with self.assertRaises(ValueError):
-            BaseMigrator('hi', 'there')
-
-        with self.assertRaises(ValueError):
-            BaseMigrator(':secret', 'xxx:yyy')
-
-    def test_create_clients(self):
         migrator = BaseMigrator(self.src, self.dst)
-        migrator._create_clients()
-        self.assertTrue(migrator.src_client)
-        self.assertTrue(migrator.dst_client)
+        self.assertEqual('BaseMigrator()', migrator.__repr__())
