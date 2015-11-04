cc2f73df3273bc8d18a7b676e1905ac94ddfbc66
Randall Degges <r@rdegges.com>
2015-11-04 11:02:50 -0800

    Implementing TenantMigrator.

--- migrate/migrators/tenant.py
+++ migrate/migrators/tenant.py
@@ -1,7 +1,7 @@
 """Our Tenant migrator."""
 
 
-from . import BaseMigrator, DirectoryMigrator
+from . import *
 
 
 class TenantMigrator(BaseMigrator):
@@ -15,12 +15,43 @@ class TenantMigrator(BaseMigrator):
 
         NOTE: This may take a longggg time to run.
         """
-        for directory in self.src_client.directories:
-            directory_migrator = DirectoryMigrator(
-                src = self.src,
-                dst = self.dst,
-                from_date = self.from_date,
-                verbose = self.verbose,
-                href = directory.href,
-            )
-            directory_migrator.migrate()
+        for directory in self.src.directories:
+            if directory.name == 'Stormpath Administrators':
+                continue
+
+            migrator = DirectoryMigrator(destination_client=self.dst, source_directory=directory)
+            destination_directory = migrator.migrate()
+
+            for group in directory.groups:
+                migrator = GroupMigrator(destination_directory=destination_directory, source_group=group)
+                migrator.migrate()
+
+            for account in directory.accounts:
+                migrator = AccountMigrator(destination_directory=destination_directory, source_account=account, source_password='asfdasAFDAFDS!!!32423')
+                migrator.migrate()
+
+                for membership in account.group_memberships:
+                    migrator = GroupMembershipMigrator(destination_client=self.dst, source_group_membership=membership)
+                    migrator.migrate()
+
+            migrator = DirectoryWorkflowMigrator(destination_directory=destination_directory, source_directory=directory)
+            migrator.migrate()
+
+        for organization in self.src.tenant.organizations:
+            migrator = OrganizationMigrator(destination_client=self.dst, source_organization=organization)
+            destination_organization = migrator.migrate()
+
+            for mapping in organization.account_store_mappings:
+                migrator = OrganizationAccountStoreMappingMigrator(destination_organization=destination_organization, source_account_store_mapping=mapping)
+                migrator.migrate()
+
+        for application in self.src.applications:
+            if application.name == 'Stormpath':
+                continue
+
+            migrator = ApplicationMigrator(destination_client=self.dst, source_application=application)
+            destination_application = migrator.migrate()
+
+            for mapping in application.account_store_mappings:
+                migrator = ApplicationAccountStoreMappingMigrator(destination_application=destination_application, source_account_store_mapping=mapping)
+                migrator.migrate()
