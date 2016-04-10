1066fefc4d37bf5de74f1dc41ef1fed181e2fc2b
Randall Degges <r@rdegges.com>
2016-04-10 15:46:42 -0700

    Adding global logger.

--- migrate/__init__.py
+++ migrate/__init__.py
@@ -1,4 +1,20 @@
 """Migrate module."""
 
 
+from logging import DEBUG, Formatter, StreamHandler, getLogger
+
+
 __version__ = '1.0.15'
+
+
+# Global logger object.  Used for controlling program output.
+logger = getLogger(__name__)
+logger.setLevel(DEBUG)
+
+log_handler = StreamHandler()
+log_handler.setLevel(DEBUG)
+
+formatter = Formatter('%(asctime)s %(name)s[%(levelname)s]: %(message)s')
+log_handler.setFormatter(formatter)
+
+logger.addHandler(log_handler)
