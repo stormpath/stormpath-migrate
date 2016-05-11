a7542e81cdc26d977c8e7278a9abe973b2543880
Randall Degges <r@rdegges.com>
2016-05-11 10:43:32 -0700

    Removing BaseMigrator.log property.

--- migrate/migrators/base.py
+++ migrate/migrators/base.py
@@ -1,15 +1,11 @@
 """Base Migrator class."""
 
 
-from .. import logger
-
-
 class BaseMigrator(object):
     """"
     This class provides a template for all other migrators.
     """
     def __init__(self, src, dst, passwords, from_date=None, verbose=False):
-        self.log = logger
         self.src = src
         self.dst = dst
         self.passwords = passwords
