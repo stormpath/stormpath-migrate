0febf04cb688d3ddbee8fef33aa6011be64acd9f
Randall Degges <r@rdegges.com>
2016-05-16 16:35:53 -0700

    Updating SubsitutionMigrator.

--- migrate/migrators/substitution.py
+++ migrate/migrators/substitution.py
@@ -1,6 +1,8 @@
 """Our Substitution Migrator."""
 
 
+from stormpath.error import Error as StormpathError
+
 from . import BaseMigrator
 from .. import logger
 
@@ -22,28 +24,32 @@ class SubstitutionMigrator(BaseMigrator):
         sc = self.source_client
         dc = self.destination_client
 
-        logger.info('Building index of Application HREFs... This may take a while.')
+        logger.info('Starting to build index of Stormpath Resource HREFs... This may take a while.')
+
         for sa in sc.applications:
             self.hrefs[sa.href] = None
 
             for app in dc.applications.search({'name': sa.name}):
                 self.hrefs[sa.href] = app.href
 
-        logger.info('Building index of Directory HREFs... This may take a while.')
+        logger.info('Finished building index of Application HREFs.')
+
         for sd in sc.directories:
             self.hrefs[sd.href] = None
 
             for dir in dc.directories.search({'name': sd.name}):
                 self.hrefs[sd.href] = dir.href
 
-        logger.info('Building index of Organization HREFs... This may take a while.')
+        logger.info('Finished building index of Directory HREFs.')
+
         for so in sc.tenant.organizations:
             self.hrefs[so.href] = None
 
             for org in dc.tenant.organizations.search({'name': so.name}):
                 self.hrefs[so.href] = org.href
 
-        logger.info('Building index of Group HREFs... This may take a while.')
+        logger.info('Finished building index of Organization HREFs.')
+
         for sg in sc.groups:
             self.hrefs[sg.href] = None
 
@@ -52,7 +58,8 @@ class SubstitutionMigrator(BaseMigrator):
                 for group in dir.groups.search({'name': sg.name}):
                     self.hrefs[sg.href] = group.href
 
-        logger.info('Building index of Account HREFs... This may take a while.')
+        logger.info('Finished building index of Group HREFs.')
+
         for sd in sc.directories:
             for sa in sd.accounts:
                 self.hrefs[sa.href] = None
@@ -61,85 +68,138 @@ class SubstitutionMigrator(BaseMigrator):
                     for account in dir.accounts.search({'email': sa.email}):
                         self.hrefs[sa.href] = account.href
 
+        logger.info('Finished building index of Account HREFs.')
+
     def migrate(self):
         """
         Rewrites all HREFs.
         """
+        dc = self.destination_client
+
         self.build_hrefs()
 
-        for app in self.destination_client.applications:
+        for app in dc.applications:
             custom_data = app.custom_data
 
-            for key, value in dict(custom_data).iteritems():
+            for key, value in dict(custom_data).items():
                 if key in self.hrefs and self.hrefs[key]:
-                    print 'Re-writing HREFs for Application:', app.name
                     del custom_data[key]
                     custom_data[self.hrefs[key]] = value
-                    custom_data.save()
+
+                    while True:
+                        try:
+                            custom_data.save()
+                            logger.info('Successfully rewrote HREF for Application: {}'.format(app.name))
+                        except StormpathError as err:
+                            logger.error('Failed to rewrite HREF for Application: {} ({})'.format(app.name, err))
 
                 if isinstance(value, str) and value in self.hrefs:
-                    print 'Re-writing HREFs for Application:', app.name
                     custom_data[key] = self.hrefs[value]
-                    custom_data.save()
 
-        for dir in self.destination_client.directories:
+                    while True:
+                        try:
+                            custom_data.save()
+                            logger.info('Successfully rewrote HREF for Application: {}'.format(app.name))
+                        except StormpathError as err:
+                            logger.error('Failed to rewrite HREF for Application: {} ({})'.format(app.name, err))
+
+        for dir in dc.directories:
             custom_data = dir.custom_data
 
-            for key, value in dict(custom_data).iteritems():
+            for key, value in dict(custom_data).items():
                 if key in self.hrefs and self.hrefs[key]:
-                    print 'Re-writing HREFs for Directory:', dir.name
                     del custom_data[key]
                     custom_data[self.hrefs[key]] = value
-                    custom_data.save()
+
+                    while True:
+                        try:
+                            custom_data.save()
+                            logger.info('Successfully rewrote HREF for Directory: {}'.format(dir.name))
+                        except StormpathError as err:
+                            logger.error('Failed to rewrite HREF for Directory: {} ({})'.format(dir.name, err))
 
                 if isinstance(value, str) and value in self.hrefs:
-                    print 'Re-writing HREFs for Directory:', dir.name
                     custom_data[key] = self.hrefs[value]
-                    custom_data.save()
 
-        for org in self.destination_client.tenant.organizations:
+                    while True:
+                        try:
+                            custom_data.save()
+                            logger.info('Successfully rewrote HREF for Directory: {}'.format(dir.name))
+                        except StormpathError as err:
+                            logger.error('Failed to rewrite HREF for Directory: {} ({})'.format(dir.name, err))
+
+        for org in dc.tenant.organizations:
             custom_data = org.custom_data
 
-            for key, value in dict(custom_data).iteritems():
+            for key, value in dict(custom_data).items():
                 if key in self.hrefs and self.hrefs[key]:
-                    print 'Re-writing HREFs for Directory:', dir.name
-                    print 'Re-writing HREFs for Organization:', org.name
                     del custom_data[key]
                     custom_data[self.hrefs[key]] = value
-                    custom_data.save()
+
+                    while True:
+                        try:
+                            custom_data.save()
+                            logger.info('Successfully rewrote HREF for Organization: {}'.format(org.name))
+                        except StormpathError as err:
+                            logger.error('Failed to rewrite HREF for Organization: {} ({})'.format(org.name, err))
 
                 if isinstance(value, str) and value in self.hrefs:
-                    print 'Re-writing HREFs for Organization:', org.name
                     custom_data[key] = self.hrefs[value]
-                    custom_data.save()
 
-        for group in self.destination_client.groups:
+                    while True:
+                        try:
+                            custom_data.save()
+                            logger.info('Successfully rewrote HREF for Organization: {}'.format(org.name))
+                        except StormpathError as err:
+                            logger.error('Failed to rewrite HREF for Organization: {} ({})'.format(org.name, err))
+
+        for group in dc.groups:
             custom_data = group.custom_data
 
-            for key, value in dict(custom_data).iteritems():
+            for key, value in dict(custom_data).items():
                 if key in self.hrefs and self.hrefs[key]:
-                    print 'Re-writing HREFs for Group:', group.name
                     del custom_data[key]
                     custom_data[self.hrefs[key]] = value
-                    custom_data.save()
+
+                    while True:
+                        try:
+                            custom_data.save()
+                            logger.info('Successfully rewrote HREF for Group: {}'.format(group.name))
+                        except StormpathError as err:
+                            logger.error('Failed to rewrite HREF for Group: {} ({})'.format(group.name, err))
 
                 if isinstance(value, str) and value in self.hrefs:
-                    print 'Re-writing HREFs for Group:', group.name
                     custom_data[key] = self.hrefs[value]
-                    custom_data.save()
 
-        for directory in self.destination_client.directories:
+                    while True:
+                        try:
+                            custom_data.save()
+                            logger.info('Successfully rewrote HREF for Group: {}'.format(group.name))
+                        except StormpathError as err:
+                            logger.error('Failed to rewrite HREF for Group: {} ({})'.format(group.name, err))
+
+        for directory in dc.directories:
             for acc in directory.accounts:
                 custom_data = acc.custom_data
 
-                for key, value in dict(custom_data).iteritems():
+                for key, value in dict(custom_data).items():
                     if key in self.hrefs and self.hrefs[key]:
-                        print 'Re-writing HREFs for Account:', acc.email
                         del custom_data[key]
                         custom_data[self.hrefs[key]] = value
-                        custom_data.save()
+
+                        while True:
+                            try:
+                                custom_data.save()
+                                logger.info('Successfully rewrote HREF for Account: {}'.format(acc.username))
+                            except StormpathError as err:
+                                logger.error('Failed to rewrite HREF for Account: {} ({})'.format(acc.username, err))
 
                     if isinstance(value, str) and value in self.hrefs:
-                        print 'Re-writing HREFs for Account:', acc.email
                         custom_data[key] = self.hrefs[value]
-                        custom_data.save()
+
+                        while True:
+                            try:
+                                custom_data.save()
+                                logger.info('Successfully rewrote HREF for Account: {}'.format(acc.username))
+                            except StormpathError as err:
+                                logger.error('Failed to rewrite HREF for Account: {} ({})'.format(acc.username, err))
