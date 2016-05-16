0a2dc0710e25a1c62dd506a2e995f49193541b10
Randall Degges <r@rdegges.com>
2016-05-16 11:37:32 -0700

    Adding more constants for cleanup.

--- migrate/constants.py
+++ migrate/constants.py
@@ -3,3 +3,12 @@
 
 # A list of Social Provider Directory IDs.
 SOCIAL_PROVIDER_IDS = ['facebook', 'google', 'linkedin', 'github']
+
+# A list of Mirror Directory IDs.
+MIRROR_PROVIDER_IDS = ['ldap', 'ad']
+
+# The Stormpath 'cloud' ID.
+STORMPATH_PROVIDER_ID = 'stormpath'
+
+# SAML ID.
+SAML_PROVIDER_ID = 'saml'
